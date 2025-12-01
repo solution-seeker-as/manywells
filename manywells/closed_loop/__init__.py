ca18a9b6cbb6182508793fc1bc8e11866d79ea70
Bjarne Grimstad <bjarne.grimstad@solutionseeker.no>
2025-12-01 14:40:47 +0100

    Add __init__.py files to manywells subfolders

---
+++ manywells/closed_loop/__init__.py
@@ -0,0 +1,8 @@
+"""
+Copyright (C) 2024 Solution Seeker AS - All Rights Reserved
+You may use, distribute and modify this code under the
+terms of the CC BY-NC 4.0 International Public License.
+
+Created 20 March 2025
+Bjarne Grimstad, bjarne.grimstad@solutionseeker.no 
+"""
