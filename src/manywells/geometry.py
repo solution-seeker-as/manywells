830897578161d542d2a63c13a62f822cbc4200f9
Bjarne Grimstad <bjarne.grimstad@solutionseeker.no>
2026-03-23 09:11:39 +0100

    Add copyright statement

--- src/manywells/geometry.py
+++ src/manywells/geometry.py
@@ -1,4 +1,8 @@
 """
+Copyright (C) 2024 Solution Seeker AS - All Rights Reserved
+You may use, distribute and modify this code under the
+terms of the CC BY-NC 4.0 International Public License.
+
 Well geometry module.
 
 Provides the WellGeometry dataclass for representing vertical and deviated
